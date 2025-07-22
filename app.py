47e76bcaf5dbe770eb99bd155207745c3727b7f7
afrath <afrathnasreen@gmail.com>
2025-07-22 14:54:17 +0530

    file submitted

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return" god is great"
+    return "<h1>its me Aiman!</h1>"
 
-if __name__=="__main__":
-    app.run(debug=True,
-            host='0.0.0.0',
-            part=5000)
+if __name__==  "__main__":
+    app.run(debug=True)
+            
